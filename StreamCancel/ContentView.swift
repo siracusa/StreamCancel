4c23bc0b5222da4ab1073c5cb51b69459081b02a
John Siracusa <siracusa@gmail.com>
2024-12-11 08:45:16 -0500

    Added explanation from Dimitri Bouniol.

--- StreamCancel/ContentView.swift
+++ StreamCancel/ContentView.swift
@@ -43,17 +43,17 @@ actor Producer {
 
             // WORKING:
 
-            let task = Task {
-                await self.doWork()
-                continuation.finish()
-            }
-
-            self.streamTask = task
-
-            continuation.onTermination = { reason in
-                print("Terminated: \(reason)")
-                task.cancel()
-            }
+            //let task = Task {
+            //    await self.doWork()
+            //    continuation.finish()
+            //}
+            //
+            //self.streamTask = task
+            //
+            //continuation.onTermination = { reason in
+            //    print("Terminated: \(reason)")
+            //    task.cancel()
+            //}
 
             // NOT WORKING:
 
@@ -67,6 +67,53 @@ actor Producer {
             //    // ERROR: Actor-isolated property 'streamTask' can not be referenced from a Sendable closure
             //    self.streamTask?.cancel()
             //}
+
+            // FIX:
+
+            self.streamTask = Task {
+                await self.doWork()
+                continuation.finish()
+            }
+
+            continuation.onTermination = { [streamTask] reason in
+                print("Terminated: \(reason)")
+                streamTask?.cancel()
+            }
+
+            // Explanation from Dimitri Bouniol as to why adding a [streamTask]
+            // capture to the NOT WORKING code, changing it to the FIX code
+            // fixes it:
+            //
+            // continuation.onTermination = { [streamTask] reason in ... }
+            //
+            // Dimitri: From a safety perspective, you copy self.streamTask into
+            // immutable storage before entering the closure that can run anywhere.
+            // This means that even if you change the task, the memory that composes
+            // where that task lives will never be in an inconsistent state.
+            //
+            // (You can access self.streamTask within that closure too if you made a
+            // helper function to retrieve the current task, but the compiler would
+            // force you to call that function with await at that point, essentially
+            // placing a lock on that actor to access it safely.)
+            //
+            // (Note that unlike Objective-C’s default, a capture list in Swift makes
+            // an explicit copy rather than a reference.)
+            //
+            // John: Ah ha, I think that's the bit of info I was missing! Though it
+            // does seem weird that it can make a full copy of the thing and then still
+            // do things with it that affect the thing that was copied! Task is a
+            // struct and therefore a value type, right?
+            //
+            // Dimitri: In that regard, I would think of Task as a thread-safe
+            // reference type. It will always refer to the same internal “Task”, though
+            // making a copy of the struct will copy the internal pointer, if you want
+            // to think of it that way.
+            //
+            // Tasks in that regard are stateless. They just point to the work that has
+            // already started, allowing you to cancel it or await its results.
+            //
+            // In a sense, it’s the same copy-on-write pattern [as Array, String, etc.],
+            // but you can’t write, so a copy never happens.
         }
     }
 
